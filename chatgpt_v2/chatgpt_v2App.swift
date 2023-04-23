758794f26f43ae86c0a4aafcf0c91ab236421da3
sher95 <sher95c@gmail.com>
2023-04-23 22:02:36 +0900

    Initial Commit

---
+++ chatgpt_v2/chatgpt_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  chatgpt_v2App.swift
+//  chatgpt_v2
+//
+//  Created by Sherzod Makhmudov on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct chatgpt_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
